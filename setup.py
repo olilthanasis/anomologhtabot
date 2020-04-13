c0174604b5d5c0e9c483591ea5b2e36765747e3e
olilthanasis <53539630+olilthanasis@users.noreply.github.com>
2020-04-14 02:24:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,13 @@
-#!/usr/bin/env python
-from __future__ import division, print_function
+from setuptools import setup
 
+setup(
+    name='insta-bot',
+    version='0.0.1',
+    packages=[''],
+    url='https://www.instagram.com/3ogelanomologhtabot/',
+    license='',
+    author='Lil Thanasis',
+    author_email='mitthan87@gmail.com',
+    description='bot'
+)
 
-def configuration(parent_package='',top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration('testing', parent_package, top_path)
-
-    config.add_subpackage('_private')
-    config.add_data_dir('tests')
-    return config
-
-if __name__ == '__main__':
-    from numpy.distutils.core import setup
-    setup(maintainer="NumPy Developers",
-          maintainer_email="numpy-dev@numpy.org",
-          description="NumPy test module",
-          url="https://www.numpy.org",
-          license="NumPy License (BSD Style)",
-          configuration=configuration,
-          )
